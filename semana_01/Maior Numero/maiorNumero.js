42e36f23e5d237e0d7ad6c358af4f9e3dca076f7
Gabriel Moura <gabrielpratamg.gm@gmail.com>
2025-05-17 14:45:16 -0300

    Maior Número - Finalizado

---
+++ semana_01/Maior Numero/maiorNumero.js
@@ -0,0 +1,36 @@
+// Criação da função para comparar todos os números inseridos
+function descobrirQualMaiorNumero() {
+  // Passado o isNan() para comparar se o valor digitado é no formato número
+  // não permitindo caracteres.
+  if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+    alert("Erro: Um ou mais campos contém um valor inválido.");
+    return;
+  }
+
+  // A primeira comparação verificar se todos os números são iguais.
+  // Ao retornar .T. ele exibe o alerta.
+  if (num1 === num2 && num2 === num3) {
+    alert("Todos os números são iguais.");
+    // Faz a verificação para ver qual número é o maior entre eles.
+  } else if (num1 >= num2 && num1 >= num3) {
+    alert(`O número ${num1} é o maior número.`);
+  } else if (num2 >= num1 && num2 >= num3) {
+    alert(`O número ${num2} é o maior número.`);
+  } else if (num3 >= num1 && num3 >= num2) {
+    alert(`O número ${num3} é o maior número`);
+  }
+}
+
+// Criado as variáveis para receberem os valores númericos.
+let num1 = Number(prompt("Insira um número aleatório: "));
+let num2 = Number(prompt("Insira novamente um número aleatório: "));
+let num3 = Number(prompt("Insira outro número aleatório: "));
+
+// Chama a função para analisar qual número é o maior entre eles.
+descobrirQualMaiorNumero(num1, num2, num3);
+
+// Demorei 1hr para realiar esta atividades.
+// Dificuldades Encontradas:
+//  * Como realizar o código;
+//  * Comparar se o dado inserido == Número;
+//  * Trabalhar com o operador AND(&&);
